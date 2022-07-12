e148c3dd735a45b9c592af1d5b81f32fdb934918
plkn <fischmechanik@gmail.com>
2022-07-12 17:22:55 +0200

    liplocket

--- cnv_thingy_liplocked.m
+++ cnv_thingy_liplocked.m
@@ -13,6 +13,7 @@ addpath(PATH_EEGLAB);
 eeglab;
 
 % Iterate datasets
+elasto_erps = [];
 for s = 1 : length(fl)
 
     % Load data
@@ -20,30 +21,36 @@ for s = 1 : length(fl)
 
     % Get id as integer
     id = str2num(fl(s).name(3 : 4));
-    
-    %loop through events
-    for speak_i = 1:length(EEG.event)
+
+    % loop through events
+    for speak_i = 1 : length(EEG.event)
         EEG.event(speak_i).speak = 'None';
     end
     nextline = 0;
     
     % write Bianca/ Kim info in extra column
-    for i = 1:length(EEG.event)
+    for e = 1 : length(EEG.event)
+
         % find COMBINE
-        if strcmp(EEG.event(i).code,'Combine') && ~isempty(EEG.event(i+1).code)
+        if strcmp(EEG.event(e).code, 'Combine') && ~isempty(EEG.event(e + 1).code)
+
             if nextline < length(EEG.event)
-                nextline = i + 1;
+                nextline = e + 1;
             end
             
             % w�hrend in dieser n�chsten zeile nach dem combine kein combine auftaucht, schreib informationen rein und gehe danach eine zeile weiter.
-            while ~strcmp(EEG.event(nextline).code,'Combine')
+            while ~strcmp(EEG.event(nextline).code, 'Combine')
                 
-                if strcmp(EEG.event(i).oldtype(end-5), '1')
-                    EEG.event(i).speak = 'Bianca';
+                if strcmp(EEG.event(e).oldtype(end - 5), '1')
+
+                    EEG.event(e).speak = 'Bianca';
                     EEG.event(nextline).speak = 'Bianca';
-                elseif strcmp(EEG.event(i).oldtype(end-5), '2')
-                    EEG.event(i).speak = 'Kim';
+
+                elseif strcmp(EEG.event(e).oldtype(end - 5), '2')
+
+                    EEG.event(e).speak = 'Kim';
                     EEG.event(nextline).speak = 'Kim';
+
                 end
                 
                 % set counter up to continue while loop
@@ -52,17 +59,19 @@ for s = 1 : length(fl)
                 else
                     break
                 end
+
                 %
-                if strcmp(EEG.event(nextline).code,'Combine')
+                if strcmp(EEG.event(nextline).code, 'Combine')
                     % proceed, until next combine is found, then repeat w new combine
                     if nextline < length(EEG.event)
-                        i = nextline; %i macht weiter, wo nextline aufh�rt
+                        e = nextline; % e macht weiter, wo nextline aufh�rt
                     else
                         break
                     end
                 end
                 
             end
+
         end
     end
     
@@ -73,75 +82,203 @@ for s = 1 : length(fl)
     end
     
     % loop through events
-    for newlat_i = 1 : length(EEG.event)
+    for e = 1 : length(EEG.event)
 
         % find S_Tr_3
-        if strcmp(EEG.event(newlat_i).oldtype, 'S_Tr_3')
+        if strcmp(EEG.event(e).oldtype, 'S_Tr_3')
 
             % if Avu, take latency from 2 lines before (check if S_Tr_1)
             % check which speaker
-            if strcmp(EEG.event(newlat_i).Av, 'Avu') && strcmp(EEG.event(newlat_i).speak, 'Bianca')
+            if strcmp(EEG.event(e).Av, 'Avu') && strcmp(EEG.event(e).speak, 'Bianca')
 
-                if strcmp(EEG.event(newlat_i-2).oldtype,  'S_Tr_1')
+                if strcmp(EEG.event(e - 2).oldtype,  'S_Tr_1')
                     % overwrite latency
                     % onset lip movement: 67 frames = 1340ms
-                    EEG.event(newlat_i).latency = EEG.event(newlat_i-2).latency + 1340;
-                elseif strcmp(EEG.event(newlat_i-3).oldtype,  'S_Tr_1')
+                    EEG.event(e).latency = EEG.event(e - 2).latency + (1340 / (1000 / EEG.srate));
+                elseif strcmp(EEG.event(e - 3).oldtype,  'S_Tr_1')
                     % overwrite latency
                     % onset lip movement: 67 frames = 1340ms
-                    EEG.event(newlat_i).latency = EEG.event(newlat_i-3).latency + 1340;
-                elseif strcmp(EEG.event(newlat_i-4).oldtype,  'S_Tr_1')
+                    EEG.event(e).latency = EEG.event(e - 3).latency + (1340 / (1000 / EEG.srate));
+                elseif strcmp(EEG.event(e - 4).oldtype,  'S_Tr_1')
                     % overwrite latency
                     % onset lip movement: 67 frames = 1340ms
-                    EEG.event(newlat_i).latency = EEG.event(newlat_i-4).latency + 1340;
+                    EEG.event(e).latency = EEG.event(e - 4).latency + (1340 / (1000 / EEG.srate));
                 else
-                    error_msg(subj,newlat_i) = 1
                     error('did not find Trigger 1 here')                    
                 end
 
-            elseif strcmp(EEG.event(newlat_i).Av, 'Avu') && strcmp(EEG.event(newlat_i).speak, 'Kim')
+            elseif strcmp(EEG.event(e).Av, 'Avu') && strcmp(EEG.event(e).speak, 'Kim')
 
-                if strcmp(EEG.event(newlat_i-2).oldtype,  'S_Tr_1')
+                if strcmp(EEG.event(e - 2).oldtype,  'S_Tr_1')
                     % overwrite latency
                     % onset lip movement: 42 frames = 840ms
-                    EEG.event(newlat_i).latency = EEG.event(newlat_i-2).latency + 840;
-                elseif strcmp(EEG.event(newlat_i-3).oldtype,  'S_Tr_1')
+                    EEG.event(e).latency = EEG.event(e - 2).latency + (840 / (1000 / EEG.srate));
+                elseif strcmp(EEG.event(e - 3).oldtype,  'S_Tr_1')
                     % overwrite latency
                     % onset lip movement: 42 frames = 840ms
-                    EEG.event(newlat_i).latency = EEG.event(newlat_i-3).latency + 840;
-                elseif strcmp(EEG.event(newlat_i-4).oldtype,  'S_Tr_1')
+                    EEG.event(e).latency = EEG.event(e - 3).latency + (840 / (1000 / EEG.srate));
+                elseif strcmp(EEG.event(e - 4).oldtype,  'S_Tr_1')
                     % overwrite latency
                     % onset lip movement: 42 frames = 840ms
-                    EEG.event(newlat_i).latency = EEG.event(newlat_i-4).latency + 840;
+                    EEG.event(e).latency = EEG.event(e - 4).latency + (840 / (1000 / EEG.srate));
                 else
-                    error_msg(subj,newlat_i) = 1
                     error('did not find Trigger 1 here')
                 end
             end
         end
     end
 
-    lat_lip = [];
-    lat_stim = [];
+    % Get SOAs for trials
+    trialinfo = [];
+    counter = 0;
+    old_cnd = '';
+    blk = 0;
     for e = 1 : length(EEG.event)
 
+        % If liponset
         if strcmpi(EEG.event(e).type, '3')
-            lat_lip(end + 1) = EEG.event(e).latency;
-        end
 
-        if strcmpi(EEG.event(e).code, 'Combine')
-            lat_stim(end + 1) = EEG.event(e).latency;
+            % Loop for tone onset
+            f = e;
+            while ~strcmpi(EEG.event(f).type, '4')
+                f = f + 1;
+            end
+
+            % Get latencies
+            lat_lip = EEG.event(e).latency;
+            lat_tone = EEG.event(f).latency;
+
+            % Save trial info
+            if EEG.event(e).trial ==  EEG.event(f).trial
+
+                % Check block
+                if ~strcmpi(EEG.event(e).Av, old_cnd)
+                    blk = blk + 1;
+                    old_cnd = EEG.event(e).Av;
+                end
+
+                % Check target position
+                if strcmpi(EEG.event(e).tarpos, 'li')
+                    tarpos = 0;
+                else
+                    tarpos = 1;
+                end
+
+                % Get conditions
+                if strcmpi(EEG.event(e).Av, 'Avk') % both
+                    cnd = 1;
+                elseif strcmpi(EEG.event(e).Av, 'Auv') % visu
+                    cnd = 2;
+                elseif strcmpi(EEG.event(e).Av, 'Avu') % audi
+                    cnd = 3;
+                end
+
+                counter = counter + 1;
+                trialinfo(counter, :) = [blk, NaN, tarpos, cnd, NaN, lat_lip, lat_tone, floor(mod(lat_lip, EEG.pnts)), floor(mod(lat_tone, EEG.pnts)), lat_lip - lat_tone];
+            end
         end
+    end
+
+    % Find standard target positions
+    for bl = 1 : 6
+        tmp = trialinfo(trialinfo(:, 1) == bl, 3);
+        if sum(tmp) < (numel(tmp) / 2)
+            trialinfo(trialinfo(:, 1) == bl, 2) = 0;
+        else
+            trialinfo(trialinfo(:, 1) == bl, 2) = 1;
+        end
+    end
+
+    % Code standard and deviant
+    trialinfo(:, 5) = ~(trialinfo(:, 2) == trialinfo(:, 3));
+
+    % Check if number of detected soas matches number of trials in data
+    if ~(EEG.trials == size(trialinfo, 1))
+        error('\n\nsomething went terribly wrong!!!!!!!!\n\n')
+    end
+
+    % Electrodes FC1, FCz, FC2, Fz, Cz as cluster
+    front_clust_idx = [19, 63, 20, 23, 16];
+
+    % Loop trials and elasto
+    n_frames = 300;
+    erp = zeros(EEG.trials, n_frames);
+    for tr = 1 : EEG.trials
+
+        % Get data
+        tmp = double(mean(EEG.data(front_clust_idx, trialinfo(tr, 8) : trialinfo(tr, 9), tr), 1));
+
+        % Get baseline
+        bl_val = double(mean(mean(EEG.data(front_clust_idx, trialinfo(tr, 8) - 100 : trialinfo(tr, 8), tr), 2), 1));
+
+        % Subtract baseline
+        tmp = tmp - bl_val;
+
+        % Resample single trial erp
+        erp(tr, :) = resample(tmp, n_frames, length(tmp));
 
     end
 
-    aa=bb
-    
-    
-    
-    
-    %EEG=pop_saveset(EEG,'filename',[SUBJECT{subj} '_cleaned_newSTr3.set'],'filepath',PATHOUT);
+    % Average across conditions
+    for cnd = 1 : 3
+        for corr = 0 : 1
+            elasto_erps(s, cnd, corr + 1, :) = mean(erp(trialinfo(:, 4) == cnd & trialinfo(:, 5) == corr, :), 1);
+        end
+    end
+
 end
+
+% Parametrize cnv in time wins
+cnv_amplitudes = [];
+counter = 0;
+for s = 1 : length(fl)
+
+    % Get id
+    id = str2num(fl(s).name(3 : 4));
+
+    % Loop conditions
+    for cnd = 1 : 3
+        for corr = 0 : 1
+            for win = 1 : 60 : 300
+
+                % Mena amplitudes
+                cnv_amp = mean(squeeze(elasto_erps(s, cnd, corr + 1, win : win + 59)));
+
+                % Save to matrix
+                counter = counter + 1;
+                cnv_amplitudes(counter, :) = [id, cnd, corr, ceil(win / 60), cnv_amp];
+
+            end
+        end
+    end
+end
+
+% Save
+dlmwrite([PATH_CLEANED, 'cnv_amplitudes.csv'], cnv_amplitudes);
+
+figure()
+subplot(1, 2, 1)
+pd = squeeze(mean(elasto_erps(:, 1, 1, :), 1))
+plot(pd, 'r', 'LineWidth', 2)
+hold on
+pd = squeeze(mean(elasto_erps(:, 2, 1, :), 1))
+plot(pd, 'g', 'LineWidth', 2)
+pd = squeeze(mean(elasto_erps(:, 3, 1, :), 1))
+plot(pd, 'k', 'LineWidth', 2)
+title('standard')
+
+subplot(1, 2, 2)
+pd = squeeze(mean(elasto_erps(:, 1, 2, :), 1))
+plot(pd, 'r', 'LineWidth', 2)
+hold on
+pd = squeeze(mean(elasto_erps(:, 2, 2, :), 1))
+plot(pd, 'g', 'LineWidth', 2)
+pd = squeeze(mean(elasto_erps(:, 3, 2, :), 1))
+plot(pd, 'k', 'LineWidth', 2)
+legend({'both', 'visu', 'audi'})
+title('deviant')
+
+
     
     
     
